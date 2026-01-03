7306f6d5e3896c7716919f7f5aec4b9e0a5f86dc
Laiba Kafayat <layibakafayat@gmail.com>
2026-01-03 15:03:57 +0500

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
+  
   theme: {
     extend: {},
   },
